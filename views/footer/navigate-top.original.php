534f0070a68bf94c7d2460bd020bf9e61e28933d
Aaron Barraza <aaron@mightyminnow.com>
2019-10-07 11:45:45 -0700

    Add screen-reader-text to go to top link.

---
+++ views/footer/navigate-top.original.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * template part for footer navigate to top module. views/footer
+ *
+ * @author 		Artbees
+ * @package 	jupiter/views
+ * @version     5.0.0
+ */
+
+?>
+
+<a href="#top-of-page" class="mk-go-top  js-smooth-scroll js-bottom-corner-btn js-bottom-corner-btn--back">
+	<?php Mk_SVG_Icons::get_svg_icon_by_class_name(true, 'mk-icon-chevron-up', 16); ?>
+	<span class="screen-reader-text"><?php _e( 'Go to top of page', 'mk_framework-child4a' ); ?></span>
+</a>
