fbddff51f69468a6fd24895f3390d6a7750efcb8
Aaron Barraza <aaron@mightyminnow.com>
2019-10-15 14:55:09 -0700

    Enqueue assets.

--- functions.php
+++ functions.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * Enqueue child theme styles and scripts.
- */
-if ( ! function_exists( 'jc_enqueue_assets' ) ) {
-	add_action( 'wp_enqueue_scripts', 'jc_enqueue_assets' );
-	function jc_enqueue_assets() {
-		wp_enqueue_style( 'jc', get_stylesheet_uri(), array(), null );
-		wp_enqueue_script( 'jc', get_stylesheet_directory_uri() . '/main.js', array( 'jquery' ), null, true );
-	}
-}
 
 /**
  * Implement "Skip to" links.
--- includes/class-jupiter-all.php
+++ includes/class-jupiter-all.php
@@ -5,12 +5,22 @@ class Jupiter_All {
 
 	public function __construct() {
 		$this->helpers();
+		$this->hooks();
 	}
 
-	public function helpers() {
+	private function helpers() {
 		include JUPITER_ALL_PATH . '/framework/helpers/global.php';
 		include JUPITER_ALL_PATH . '/framework/helpers/wp_head.php';
 		include JUPITER_ALL_PATH . '/includes/helpers/template-part-helpers.php';
+	}
+
+	private function hooks() {
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_assets' ) );
+	}
+
+	public function enqueue_assets() {
+		wp_enqueue_style( 'j4a', JUPITER_ALL_URL . '/css/j4a.css', array(), null );
+		wp_enqueue_script( 'j4a', JUPITER_ALL_URL . '/js/j4a.js', array( 'jquery' ), null, true );
 	}
 
 }
