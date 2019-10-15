2da1109a602fa4bf0e2eec23c0403708f7aa401c
Aaron Barraza <aaron@mightyminnow.com>
2019-10-15 15:01:05 -0700

    Update assets URL. Delete functions.php

--- includes/class-jupiter-all.php
+++ includes/class-jupiter-all.php
@@ -16,11 +16,16 @@ class Jupiter_All {
 
 	private function hooks() {
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_assets' ) );
+		add_action( 'mk_theme_after_body_opening', array( $this, 'skip_to_links' ) );
 	}
 
 	public function enqueue_assets() {
-		wp_enqueue_style( 'j4a', JUPITER_ALL_URL . '/css/j4a.css', array(), null );
-		wp_enqueue_script( 'j4a', JUPITER_ALL_URL . '/js/j4a.js', array( 'jquery' ), null, true );
+		wp_enqueue_style( 'j4a', JUPITER_ALL_URL . 'css/j4a.css', array(), null );
+		wp_enqueue_script( 'j4a', JUPITER_ALL_URL . 'js/j4a.js', array( 'jquery' ), null, true );
+	}
+
+	public function skip_to_links() {
+		mk_get_template_part( 'parts/header/global/skip-to-links' );
 	}
 
 }
