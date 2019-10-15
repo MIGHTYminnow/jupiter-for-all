46eca67725a029b110f589332e520cebe7ff637e
Aaron Barraza <aaron@mightyminnow.com>
2019-10-15 15:05:02 -0700

    Add skip to footer link.

--- parts/header/global/skip-to-links.php
+++ parts/header/global/skip-to-links.php
@@ -1,1 +1,2 @@
- <a class="screen-reader-shortcut" href="#theme-page">Skip to main content</a> 
+<a class="screen-reader-shortcut" href="#theme-page">Skip to main content</a> 
+<a class="screen-reader-shortcut" href="#mk-footer">Skip to footer</a>
