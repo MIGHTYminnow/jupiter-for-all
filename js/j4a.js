5b1e0b7a7156441afbe46d9cb1a09527f1dc4e01
Aaron Barraza <aaron@mightyminnow.com>
2019-10-23 18:04:26 -0700

    Add title attribute to Google Maps iframes.

--- js/j4a.js
+++ js/j4a.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($){
 	/**
-	 * Implement aria-expanded on the mobile menu button.
+	 * Implements aria-expanded on the mobile menu button.
 	 */
 	$( '.mk-nav-responsive-link' ).on( 'click', function() {
 		if ( $(this).attr( 'aria-expanded' ) === 'false' ) {
@@ -11,11 +11,14 @@ jQuery(document).ready(function($){
 	});
 
 	/**
-	 * Add title attribute to Youtube iframes.
+	 * Adds title attribute to Youtube and Google Maps iframes.
 	 */
 	$( 'iframe' ).each(function(){
-		if ( $( this ).attr( 'src' ).includes( 'youtube.com' ) ) {
+		var src = $( this ).attr( 'src' );
+		if ( src.includes( 'youtube.com' ) ) {
 			$( this ).attr( 'title', 'Youtube video' );
+		} else if ( src.includes( 'google.com/maps/embed' ) ) {
+			$( this ).attr( 'title', 'Google Maps' );
 		}
 	});
 });
