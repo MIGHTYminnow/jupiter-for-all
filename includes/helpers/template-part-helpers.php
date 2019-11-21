f8555e65c3c7c5b074029ea461e3212993805351
Aaron Barraza <aaron@mightyminnow.com>
2019-11-20 18:18:12 -0700

    Override mk_get_shortcode_view(). Allows to override Jupiter shortcode views from Jupiter for All and child themes.

--- includes/helpers/template-part-helpers.php
+++ includes/helpers/template-part-helpers.php
@@ -1,5 +1,39 @@
 <?php
 /**
+ * Overrides mk_get_shortcode_view()
+ * from /includes/helpers/template-part-helpers.php on Jupiter Donut.
+ * 
+ * @version 1.0.2
+ */
+
+/**
+ * Allows to override Jupiter shortcode views from Jupiter for All and child themes.
+ */
+if ( ! function_exists( 'mk_get_shortcode_view' ) ) {
+	function mk_get_shortcode_view( $shortcode_name, $name = '', $return = false, $view_params = array() ) {
+
+		if ( is_dir( get_stylesheet_directory() . '/components/shortcodes/' . $shortcode_name ) ) {
+			$directory = 'components';
+		} elseif ( is_dir( JUPITER_ALL_PATH . '/wpbakery/shortcodes/' . $shortcode_name ) ) {
+			$directory = 'wpbakery';
+		} elseif ( is_dir( JUPITER_DONUT_INCLUDES_DIR . '/wpbakery/shortcodes/' . $shortcode_name ) ) {
+			$directory = 'wpbakery';
+		} elseif ( is_dir( get_template_directory() . '/components/shortcodes/' . $shortcode_name ) ) {
+			$directory = 'components';
+		}
+
+		if ( $return ) {
+			ob_start();
+			mk_get_template_part( $directory . '/shortcodes/' . $shortcode_name . '/' . $name, $view_params );
+			return ob_get_clean();
+		} else {
+			mk_get_template_part( $directory . '/shortcodes/' . $shortcode_name . '/' . $name, $view_params );
+		}
+
+	}
+}
+
+/**
  * Overrides mk_get_template_part()
  * from /includes/helpers/template-part-helpers.php on Jupiter Donut.
  * 
