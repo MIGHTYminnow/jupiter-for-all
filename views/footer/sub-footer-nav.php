635aa99ad06d282ed9a63d4f1d86b5f14f9f979d
Aaron Barraza <aaron@mightyminnow.com>
2019-11-21 13:20:06 -0700

    Accessibility Fix: Ensures landmarks are unique.
    Add aria-label to navigation menus.

---
+++ views/footer/sub-footer-nav.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * template part for sub-footer navigation. views/footer
+ *
+ * @author 		Artbees
+ * @package 	jupiter/views
+ * @version     5.0.0
+ */
+
+
+$footer_menu = wp_nav_menu(array(
+    'theme_location' => 'footer-menu',
+    'container' => 'nav',
+    'container_id' => 'mk-footer-navigation',
+    'container_class' => 'footer_menu',
+    'fallback_cb' => '',
+	'echo' => false,
+));
+
+echo str_replace( '<nav', '<nav aria-label="Footer Navigation"', $footer_menu );
