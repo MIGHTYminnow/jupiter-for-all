cc232dfa88161678956e841c6c4eb42ff36371dc
Aaron Barraza <hola@aaronbarraza.mx>
2020-03-23 04:59:26 -0700

    Fix: Content not included in landmarks.

--- parts/header/global/skip-to-links.php
+++ parts/header/global/skip-to-links.php
@@ -1,2 +1,4 @@
-<a class="screen-reader-shortcut" href="#theme-page">Skip to main content</a> 
-<a class="screen-reader-shortcut" href="#mk-footer">Skip to footer</a>
+<nav aria-label="<?php _e( 'Skip to:', 'j4a' ); ?>">
+	<a class="screen-reader-shortcut" href="#theme-page">Skip to main content</a> 
+	<a class="screen-reader-shortcut" href="#mk-footer">Skip to footer</a>
+</nav>
