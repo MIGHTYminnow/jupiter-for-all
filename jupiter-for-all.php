1cd3fbad70db7044cd0235c93f885a7f0a234cde
Aaron Barraza <hola@aaron.mx>
2021-03-12 13:05:01 -0700

    [TAG] Version 1.1.8

--- jupiter-for-all.php
+++ jupiter-for-all.php
@@ -3,7 +3,7 @@
  * Plugin Name: Jupiter for All
  * Plugin URI: https://github.com/MIGHTYminnow/jupiter-child-for-all
  * Description: Makes your Jupiter child theme accessible.
- * Version: 1.1.7
+ * Version: 1.1.8
  * Author: MIGHTYminnow
  * Author URI: https://mightyminnow.com
  */
