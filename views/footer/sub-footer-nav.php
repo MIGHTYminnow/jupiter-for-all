5ef0578c27a37d647e9cbaccf64334778e4dd6de
Aaron Barraza <aaron@mightyminnow.com>
2024-05-27 11:04:38 -0700

    Re-apply customizations to sub-footer-nav.php

--- views/footer/sub-footer-nav.php
+++ views/footer/sub-footer-nav.php
@@ -6,13 +6,18 @@
  * @author 		Artbees
  * @package 	jupiter/views
  * @version     5.0.0
+ * 
+ * Jupiter Version: 6.12.0
  */
 
 
-wp_nav_menu(array(
+$footer_menu = wp_nav_menu(array(
     'theme_location' => 'footer-menu',
     'container' => 'nav',
     'container_id' => 'mk-footer-navigation',
     'container_class' => 'footer_menu',
     'fallback_cb' => '',
+	'echo' => false,
 ));
+
+echo str_replace( '<nav', '<nav aria-label="Footer Navigation"', $footer_menu );
