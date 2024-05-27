b46b1d708d05a3e5151f9fbba145ec3b7cc9315c
Aaron Barraza <aaron@mightyminnow.com>
2024-05-27 11:14:00 -0700

    Re-apply customizations to full-screen-search.php

--- views/header/global/full-screen-search.php
+++ views/header/global/full-screen-search.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Overrides /views/header/global/full-screen-search.php on Jupiter.
+ * 
+ * @version 6.12.0
+ */
 
 /**
  * template part for full screen search form. views/header/global
@@ -11,11 +16,14 @@
 ?>
 
 <div class="mk-fullscreen-search-overlay">
-	<a href="#" class="mk-fullscreen-close"><?php Mk_SVG_Icons::get_svg_icon_by_class_name(true, 'mk-moon-close-2'); ?></a>
+	<a href="#" class="mk-fullscreen-close">
+		<?php Mk_SVG_Icons::get_svg_icon_by_class_name(true, 'mk-moon-close-2'); ?>
+		<span class="screen-reader-text"><?php _e( 'Close Search', 'mk_framework' ); ?></span>
+	</a>
 	<div class="mk-fullscreen-search-wrapper">
 		<p><?php _e('Start typing and press Enter to search', 'mk_framework'); ?></p>
 		<form method="get" id="mk-fullscreen-searchform" action="<?php echo home_url('/'); ?>">
-			<input type="text" value="" name="s" id="mk-fullscreen-search-input" />
+			<input type="text" value="" name="s" id="mk-fullscreen-search-input" aria-label="<?php _e( 'Search', 'mk_framework' ); ?>" />
 			<i class="fullscreen-search-icon"><?php Mk_SVG_Icons::get_svg_icon_by_class_name(true, 'mk-icon-search', 25); ?></i>
 		</form>
 	</div>
