04cc040cf38524f4869ac5a986c65efcbb36e632
Aaron Barraza <aaron@mightyminnow.com>
2024-05-27 11:25:42 -0700

    Re-apply customizations to main-menu-burger-icon.php

--- views/header/global/main-menu-burger-icon.php
+++ views/header/global/main-menu-burger-icon.php
@@ -1,4 +1,10 @@
 <?php 
+/**
+ * Overrides /views/header/global/main-menu-burger-icon.php on Jupiter.
+ * 
+ * @version 6.12.0
+ */
+
 global $mk_options;
 
 if ($view_params['header_style'] == 3) return false;
@@ -7,10 +13,11 @@ if(isset($mk_options['hide_header_nav']) && $mk_options['hide_header_nav'] == 'f
 
 ?>
 
-<div class="mk-nav-responsive-link">
+<button class="mk-nav-responsive-link" aria-expanded="false">
     <div class="mk-css-icon-menu">
         <div class="mk-css-icon-menu-line-1"></div>
         <div class="mk-css-icon-menu-line-2"></div>
         <div class="mk-css-icon-menu-line-3"></div>
+		<span class="screen-reader-text"><?php _e( 'Main navigation', 'jupiter-child' ); ?></span>
     </div>
-</div>
+</button>
