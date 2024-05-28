b6a3adc2904ce4512d4f0b4e99414f521a5b2a4d
Aaron Barraza <aaron@mightyminnow.com>
2024-05-28 04:27:54 -0700

    Remove the template overrides of wpbakery shortcodes
    They were not being loaded for a while and they have changed a lot at this point.

--- wpbakery/shortcodes/mk_employees/components/content.php
+++
@@ -1,5 +0,0 @@
-<?php if ($view_params['description'] == 'true' && ! empty( get_post_meta(get_the_ID() , '_desc', true) ) ) {
-		$content = str_replace(']]>', ']]&gt;', apply_filters('the_content', get_post_meta(get_the_ID() , '_desc', true)));
-?>
-		<div class="team-member-desc jupiter-donut-margin-top-20 jupiter-donut-margin-bottom-10 jupiter-donut-display-block"><?php echo $content; ?></div>
-<?php } ?>
